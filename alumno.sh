323aa29a54b228120c18f3034bc19fa5a415d197
Alfonso Nava <alfonsonava253@gmail.com>
2024-10-01 04:24:07 +0000

    script

---
+++ alumno.sh
@@ -0,0 +1,16 @@
+echo "Introduzca el nombre:"
+read nombre
+echo "Introduzca la edad:"
+read edad
+echo "Introduzca la carrera:"
+read carrera
+echo "Introduzca la matricula:"
+read matricula
+
+echo "Nombre: alfonso Nava" > alumno.txt
+echo "edad: 23" >> alumno.txt
+echo "carrera: Informatica" >> alumno.txt
+echo "Matricula: 10048998" >> alumno.txt
+
+echo "Datos guardados exitosamente en alumno.txt"
+
